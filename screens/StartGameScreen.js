1e623fbdbb6e1badf650158dcc85e3dae7a90a62
EmiliaMlak <emiliamlak@gmail.com>
2021-12-20 19:29:26 +0000

    add more styling to inputContainer in StartGameScreen component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import { shadowOffset } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 const StartGameScreen = props => {
     return (
@@ -30,7 +31,15 @@ const styles = StyleSheet.create({
     inputContainer: {
         width: 300,
         maxWidth: '80%',
-        alignItems: 'center'
+        alignItems: 'center',
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        elevation: 5,
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10
     },
     buttonContainer: {
         flexDirection: 'row',
